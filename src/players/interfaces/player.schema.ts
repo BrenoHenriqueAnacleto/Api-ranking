b0384108a9de602439df5faf1b0312351c9405df
Breno Henrique Anacleto <brenohenriqueanacleto@gmail.com>
2022-07-12 14:13:22 -0300

    Criação da entidade player

---
+++ src/players/interfaces/player.schema.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+
+export const PlayerSchema = new mongoose.Schema({
+    email: {type:String, unique:true},
+    phoneNumber: {type: String},
+    nome: {type:String},
+    ranking: {type: String},
+    rankingPosition: {type:Number},
+    urlPlayerPhoto: {type: String}
+}, {timestamps: true, collection: 'players'});
---
+++ src/players/pipes/players-validation-parameters.pipe.ts
@@ -0,0 +1,13 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+
+export class PlayersValidationParametersPipe implements PipeTransform {
+
+    transform(value: any, metadata: ArgumentMetadata) {
+
+        if (!value) {
+            throw new BadRequestException(`O valor do parametro ${metadata.data} deve ser informado.`)
+        }
+
+        return value
+    }
+}
